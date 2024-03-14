f241bf3b76b3090a2e5dcd2d9d13dc6688859e25
hazly25 <hazlyjhoanalarreacastillo@gmail.com>
2024-03-14 13:55:57 -0500

    Ejemplo 2 Bill

--- ProyectoConstruccion2/src/main/java/app/dao/BillDaoImp.java
+++ ProyectoConstruccion2/src/main/java/app/dao/BillDaoImp.java
@@ -4,29 +4,5 @@ import app.dto.PersonDto;
 
 public class BillDaoImp {
 	
-	public void createOwner() throws Exception{
-		System.out.println("Ingrese el nombre completo");
-		String fullName = reader.nextLine();
-		personInputsValidator.fullNameValidator(fullName);
-		
-		System.out.println("Ingrese el número de documento");
-		long id = personInputsValidator.idValidator(reader.nextLine());
-		
-		
-		System.out.println("Ingrese la edad");
-		int age = personInputsValidator.ageValidator(reader.nextLine());
-		
-		String rol ="owner";
-		
-		String userName ="N/A";
-		
-		String password ="N/A";
-		
-		
-		PersonDto personDto = new PersonDto(id, fullName, age, rol, userName, password);
-		VeterinarianService.createOwner(personDto);
-		//Duda: no necesito los otros parametros, entonces que hago para crear el dueño
-		//Toca preguntarle al profe como hacemos que esos parametros no sean necesarios ya que por el 
-		//no deja
-	}
+	//Verificar que el cabio si guardo
 }
