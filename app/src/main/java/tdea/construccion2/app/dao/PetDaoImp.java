13a68f9ebd8967d1ea58958858fa307e7857a368
hazly25 <hazlyjhoanalarreacastillo@gmail.com>
2024-05-11 13:04:42 -0500

    Cambios 2 Mayo10

--- app/src/main/java/tdea/construccion2/app/dao/PetDaoImp.java
+++ app/src/main/java/tdea/construccion2/app/dao/PetDaoImp.java
@@ -8,44 +8,49 @@ import tdea.construccion2.app.repository.PetRepository;
 
 @Service
 public class PetDaoImp implements PetDao{
-	
 	@Autowired
 	private PetRepository petRepository;
 	
-	
+	@Override
 	public void createPet(PetDto petDto) throws Exception {
 		Pet pet = new Pet(petDto);
 		petRepository.save(pet);
 	}
 
-	public boolean findPetBynamePet(PetDto petDto) throws Exception {
-		return petRepository.findPetExist(petDto.getNamePet());
+        @Override
+	public boolean findexistById(PetDto petDto) throws Exception {
+		return petRepository.existById(petDto.getIdPet());
 	}
 
 	public PetDto findPetById(PetDto petDto) throws Exception {
-		Pet pet = petRepository.findPetById(petDto.getIdPet());
+		Pet pet = petRepository.findByidPet(petDto.getIdPet());
 		if(pet == null) {
 			return null;
 		}
 		return new PetDto(pet);
 		
 	}
-	
+       
+        @Override 
+        public boolean existdBynamePet(PetDto petDto) throws Exception{
+            return petRepository.existBynamePet(petDto.getNamePet());
+        }
+        
 	@Override
-	public PetDto findPet(PetDto petDto) throws Exception {
-		Pet pet = petRepository.findPet(petDto.getNamePet());
-		if(pet == null) {
-			return null;
-		}else {
-			return new PetDto(pet);
-		}
+	public PetDto findBynamePet(PetDto petDto) throws Exception {
+            Pet pet = petRepository.findBynamePet(petDto.getNamePet());
+            if(pet == null) {
+		return null;
+            }else {
+		return new PetDto(pet);
+            }
 	}
 
 	public PetRepository getPetRepository() {
-		return petRepository;
+            return petRepository;
 	}
 
 	public void setPetRepository(PetRepository petRepository) {
-		this.petRepository = petRepository;
+            this.petRepository = petRepository;
 	}
 }
--- app/src/main/java/tdea/construccion2/app/repository/PetRepository.java
+++ app/src/main/java/tdea/construccion2/app/repository/PetRepository.java
@@ -6,8 +6,9 @@ import tdea.construccion2.app.models.Pet;
 
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long>{
+        public boolean existBynamePet(String namePet);
 	public Pet findBynamePet(String pet);
-	public Pet findPetById(long id);
-	public boolean findPetExist(String pet);
+        public boolean existById(long id);
+	public Pet findByidPet(long id);	
 	
 }
